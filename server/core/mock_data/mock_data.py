8516da4dab1ef04fdb2567897c616f5974a55834
chintan.thakkar <201701141@daiict.ac.in>
2022-08-02 16:33:04 +0530

    added migration file to create mock data

---
+++ server/core/mock_data/mock_data.py
@@ -0,0 +1,181 @@
+blogs = [
+    {
+        "blog_title": "Stack Data Structure",
+        "published_date": "2022-08-01",
+        "resources": [
+            {
+                "resource_type": "Video",
+                "resource_link": "https://www.youtube.com/embed/_6COl6V6mng",
+                "resource_text": "",
+            },
+            {
+                "resource_type": "Video",
+                "resource_link": "https://www.youtube.com/embed/BmZnJehDzyU",
+                "resource_text": "",
+            },
+            {
+                "resource_type": "Video",
+                "resource_link": "https://www.youtube.com/embed/lJLcqDsmYfg",
+                "resource_text": "",
+            },
+            {
+                "resource_type": "Video",
+                "resource_link": "https://www.youtube.com/embed/9u2BJfmWNEg",
+                "resource_text": "",
+            },
+            {
+                "resource_type": "Text",
+                "resource_link": "",
+                "resource_text": "Stack is a linear data structure which follows a particular order in which the operations are performed. The order may be LIFO(Last In First Out) or FILO(First In Last Out). There are many real-life examples of a stack. Consider an example of plates stacked over one another in the canteen. The plate which is at the top is the first one to be removed, i.e. the plate which has been placed at the bottommost position remains in the stack for the longest period of time. So, it can be simply seen to follow LIFO(Last In First Out)/FILO(First In Last Out) order. There are many real-life examples of a stack. Consider an example of plates stacked over one another in the canteen. The plate which is at the top is the first one to be removed, i.e. the plate which has been placed at the bottommost position remains in the stack for the longest period of time. So, it can be simply seen to follow LIFO(Last In First Out)/FILO(First In Last Out) order.",
+            },
+            {
+                "resource_type": "Text",
+                "resource_link": "",
+                "resource_text": "In order to make manipulations in a stack, there are certain operations provided to us. When we want to insert an element into the stack the operation is known as the push operation whereas when we want to remove an element from the stack the operation is known as the pop operation. If we try to pop from an empty stack then it is known as underflow and if we try to push an element in a stack that is already full, then it is known as overflow.",
+            },
+        ],
+    },
+    {
+        "blog_title": "Queue Data Structure",
+        "published_date": "2022-08-01",
+        "resources": [
+            {
+                "resource_type": "Video",
+                "resource_link": "https://www.youtube.com/embed/W7uB9-TKfTg",
+                "resource_text": "",
+            },
+            {
+                "resource_type": "Video",
+                "resource_link": "https://www.youtube.com/embed/_gJ3to4RyeQ",
+                "resource_text": "",
+            },
+            {
+                "resource_type": "Video",
+                "resource_link": "https://www.youtube.com/embed/UbAEP7P0vfk",
+                "resource_text": "",
+            },
+            {
+                "resource_type": "Video",
+                "resource_link": "https://www.youtube.com/embed/va_6RmSrKCg",
+                "resource_text": "",
+            },
+            {
+                "resource_type": "Text",
+                "resource_link": "",
+                "resource_text": "Queue is a linear data structure that follows a particular order in which the operations are performed for storing data. The order is First In First Out (FIFO).  One can imagine a queue as a line of people waiting to receive something in sequential order which starts from the beginning of the line. It is an ordered list in which insertions are done at one end which is known as the rear and deletions are done from the other end known as the front. A good example of a queue is any queue of consumers for a resource where the consumer that came first is served first. The difference between stacks and queues is in removing.",
+            },
+        ],
+    },
+    {
+        "blog_title": "Tree Data Structure",
+        "published_date": "2022-08-01",
+        "resources": [
+            {
+                "resource_type": "Video",
+                "resource_link": "https://www.youtube.com/embed/5NiXlPrLslg",
+                "resource_text": "",
+            },
+            {
+                "resource_type": "Video",
+                "resource_link": "https://www.youtube.com/embed/nHMQ33LZ6oA",
+                "resource_text": "",
+            },
+            {
+                "resource_type": "Video",
+                "resource_link": "https://www.youtube.com/embed/s1d8UGDCCN8",
+                "resource_text": "",
+            },
+            {
+                "resource_type": "Video",
+                "resource_link": "https://www.youtube.com/embed/QG0hE0R_ng4",
+                "resource_text": "",
+            },
+            {
+                "resource_type": "Video",
+                "resource_link": "https://www.youtube.com/embed/ffE1xj51EBQ",
+                "resource_text": "",
+            },
+            {
+                "resource_type": "Video",
+                "resource_link": "https://www.youtube.com/embed/XLdpy0_6MR4",
+                "resource_text": "",
+            },
+            {
+                "resource_type": "Video",
+                "resource_link": "https://www.youtube.com/embed/2BdY9fixMrM",
+                "resource_text": "",
+            },
+            {
+                "resource_type": "Text",
+                "resource_link": "",
+                "resource_text": "A tree is a popular data structure that is non-linear in nature. Unlike other data structures like array, stack, queue, and linked list which are linear in nature, a tree represents a hierarchical structure. The ordering information of a tree is not important. A tree contains nodes and 2 pointers. These two pointers are the left child and the right child of the parent node.",
+            },
+            {
+                "resource_type": "Text",
+                "resource_link": "",
+                "resource_text": "Binary Tree: A tree whose elements have at most 2 children is called a binary tree. Since each element in a binary tree can have only 2 children, we typically name them the left and right child. ",
+            },
+            {
+                "resource_type": "Text",
+                "resource_link": "",
+                "resource_text": "Binary Tree Representation: A tree is represented by a pointer to the topmost node of the tree. If the tree is empty, then the value of the root is NULL.",
+            },
+        ],
+    },
+    {
+        "blog_title": "Graph Data Structure",
+        "published_date": "2022-08-01",
+        "resources": [
+            {
+                "resource_type": "Video",
+                "resource_link": "https://www.youtube.com/embed/EaK6aslcC5g",
+                "resource_text": "",
+            },
+            {
+                "resource_type": "Video",
+                "resource_link": "https://www.youtube.com/embed/b5kij1Akf9I",
+                "resource_text": "",
+            },
+            {
+                "resource_type": "Video",
+                "resource_link": "https://www.youtube.com/embed/aJa3U-hydXc",
+                "resource_text": "",
+            },
+            {
+                "resource_type": "Video",
+                "resource_link": "https://www.youtube.com/embed/1cSzxlhxOw8",
+                "resource_text": "",
+            },
+            {
+                "resource_type": "Video",
+                "resource_link": "https://www.youtube.com/embed/T_boOrr0rvk",
+                "resource_text": "",
+            },
+            {
+                "resource_type": "Video",
+                "resource_link": "https://www.youtube.com/embed/6XmzL04mlgQ",
+                "resource_text": "",
+            },
+            {
+                "resource_type": "Video",
+                "resource_link": "https://www.youtube.com/embed/X2_tYUuthH8",
+                "resource_text": "",
+            },
+            {
+                "resource_type": "Text",
+                "resource_link": "",
+                "resource_text": "A Graph is a non-linear data structure consisting of nodes and edges. The nodes are sometimes also referred to as vertices and the edges are lines or arcs that connect any two nodes in the graph.",
+            },
+            {
+                "resource_type": "Text",
+                "resource_link": "",
+                "resource_text": "Graphs are used to solve many real-life problems. Graphs are used to represent networks. The networks may include paths in a city or telephone network or circuit network. Graphs are also used in social networks like linkedIn, Facebook. For example, in Facebook, each person is represented with a vertex(or node). Each node is a structure and contains information like person id, name, gender, locale etc.",
+            },
+            {
+                "resource_type": "Text",
+                "resource_link": "",
+                "resource_text": "Binary Tree Representation: A tree is represented by a pointer to the topmost node of the tree. If the tree is empty, then the value of the root is NULL.",
+            },
+        ],
+    },
+]
---
+++ server/core/mock_data/user_mock_data.py
@@ -0,0 +1,5 @@
+users = [
+    {"user_name": "Jay", "email": "jay@gmail.com", "password": "123"},
+    {"user_name": "Max", "email": "chintan@gmail.com", "password": "123"},
+    {"user_name": "Omega", "email": "omega@gmail.com", "password": "123"},
+]
