90bc0fd7b341f93462a3a3dacd3ae14862423ac8
chintan.thakkar <201701141@daiict.ac.in>
2022-08-02 16:30:51 +0530

    created component to view resource details

--- client/src/views/ResourceScreen/components/ResourceDetails.js
+++ client/src/views/ResourceScreen/components/ResourceDetails.js
@@ -1,39 +1,63 @@
-import { Card, Row, Col, Button, Alert } from "antd";
+import { Alert, Progress, Typography, Button, message, Row, Col } from "antd";
 import React from "react";
+import { useMutation } from "@apollo/client";
+import { ENROLL_IN_BLOG } from "../../../services/mutations";
+import ResourceTemplate from "../../../components/ResourceTemplate";
+import { BLOG_DETAILS } from "../../../services/queries";
+const { Title } = Typography;
 
 function ResourceDetails({ data }) {
+  const [enrollInBlog] = useMutation(ENROLL_IN_BLOG, {
+    onCompleted(data) {
+      data?.enrollInBlog?.ok
+        ? message.success(data?.enrollInBlog?.message)
+        : message.error(data?.enrollInBlog?.message);
+    },
+    refetchQueries: [
+      {
+        query: BLOG_DETAILS,
+        variables: { filtering: { userId: 2, blogId: data?.blogDetails[0]?.blogId  } },
+      },
+    ],
+  });
+  function handleEnroll(blogId) {
+    enrollInBlog({
+      variables: {
+        blogId: blogId,
+        userId: 2,
+      },
+    });
+  }
   return (
     <>
+    <Title level={2}>Resources</Title>
       {!data?.blogDetails[0]?.isEnrolledInBlog && (
         <Alert
           type="info"
-          description="Please Enroll in Blog to view Resources"
-        />
+          description={
+          <Row>
+            <Col span={8}>Please Enroll in Blog to view Resources</Col>
+            <Col span={8}></Col>
+            <Col span={8} style={{display: 'flex', justifyContent: 'flex-end'}}>          
+              <Button
+              type="primary"
+              onClick={() => handleEnroll(data?.blogDetails[0]?.blogId)}
+              >
+              Enroll to View
+              </Button>
+            </Col>
+          </Row>}
+        >
+        </Alert>
       )}
-      <Card>
-        {data?.blogDetails[0]?.resources?.map((resource) => (
-          <Card>
-            <Row>
-              <Col span={2}>{resource?.resourceId}</Col>
-              <Col span={4}>{resource?.resourceType}</Col>
-              <Col span={10}>{resource?.resourceLink}</Col>
-              <Col span={4}>
-                <Button
-                  type="primary"
-                  disabled={!data?.blogDetails[0]?.isEnrolledInBlog}
-                >
-                  Open
-                </Button>
-              </Col>
-              {data?.blogDetails[0]?.isEnrolledInBlog && (
-                <Col span={4}>
-                  {parseInt(resource?.resourceProgressPercentage)}%
-                </Col>
-              )}
-            </Row>
-          </Card>
-        ))}
-      </Card>
+
+      {data?.blogDetails[0]?.isEnrolledInBlog && (<Progress
+        style={{ top: '0px', width: '70vw' }}
+        percent={data?.blogDetails[0]?.blogProgressPercentage}
+        status="active"
+      />)}
+
+      {data?.blogDetails[0]?.resources?.map((resource) => <ResourceTemplate resource={resource} disabled={!data?.blogDetails[0]?.isEnrolledInBlog} blogId={data?.blogDetails[0]?.blogId} />)}
     </>
   );
 }
